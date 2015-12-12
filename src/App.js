8ed5ddd82a679c9a2825de4eddf83ed649849162
8845musign <8845musign@gmail.com>
2015-12-12 19:38:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Component from "./Component.jsx";
+
+ReactDOM.render(
+    React.createElement(Component),
+    document.getElementById('app')
+)
